f1bda53dbf2a29243aa35d11eb38b1f942307310
romulogmp <romulo.mendonca.pereira@everis.com>
2020-01-17 21:14:03 -0300

    first commit

---
+++ backend/src/controllers/DevController.js
@@ -0,0 +1,122 @@
+const axios = require('axios');
+const Dev = require('../models/Dev');
+const parseStringAsArray = require('../utils/parseStringAsArray');
+
+module.exports = {
+
+    async index(request, response){
+        const devs = await Dev.find();
+
+        return response.json(devs);
+    },
+
+    async store(request, response) {
+        
+        const{ github_username, techs, latitude, longitude } = request.body;
+
+        let dev = await Dev.findOne({ github_username });
+
+        if(!dev){
+
+            const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+
+            const { name = login, avatar_url, bio } = apiResponse.data;
+
+            const techsArray = parseStringAsArray(techs);
+
+            const location = {
+                type: "Point",
+                coordinates: [longitude, latitude]
+            }
+
+            dev = await Dev.create({
+                github_username,
+                name,
+                avatar_url,
+                bio,
+                techs: techsArray,
+                location
+            });
+        }
+        return response.json(dev);
+    },
+
+    async update(request, response){
+
+        try{
+
+            const { github_username } = request.query;
+
+            const { bio, techs, avatar_url, longitude, latitude } = request.body;
+
+            let techsArray;
+
+            if(techs){
+                techsArray = parseStringAsArray(techs);
+            }
+
+            let location;
+
+            if(longitude && latitude){
+                location = {
+                    type: "Point",
+                    coordinates: [longitude, latitude]
+                }
+            }
+
+            const conditions = { github_username: github_username };
+
+            const update = {
+                avatar_url,
+                bio,
+                techs: techsArray,
+                location
+            };
+
+            const options = {
+                new: true,
+                omitUndefined: true
+            };
+
+            const dev = await Dev.findOneAndUpdate(conditions, update, options);
+
+            if (!dev) {
+
+                return response.status(404).send();
+
+              }
+        
+            return response.status(200).json(dev);
+
+        } catch (error) {
+
+            response.status(400).send(error);
+
+        }
+    },
+
+    async destroy(request, response){
+
+        try{
+
+            const { github_username } = request.query;
+
+            const conditions = { github_username: github_username };
+
+            const dev = await Dev.findOneAndDelete(conditions);
+
+            if (!dev) {
+            
+                return response.status(404).send();
+
+              }
+        
+            return response.status(200).json(dev);
+        
+        } catch (error) {
+
+            response.status(400).send(error);
+
+        }
+    }
+};
---
+++ backend/src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('INSERT DB URL', {
+    useNewUrlParser : true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+});
+
+mongoose.set('useFindAndModify', false);
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
