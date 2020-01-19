9de76cb6983805212ae3cc6a31d7af930564ebbd
romulogmp <romulo.mendonca.pereira@everis.com>
2020-01-19 10:48:51 -0300

    "commit WebSocket

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
 const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
+
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect('INSERT DB URL', {
     useNewUrlParser : true,
@@ -10,9 +17,11 @@ mongoose.connect('INSERT DB URL', {
     useCreateIndex: true
 });
 
+
 mongoose.set('useFindAndModify', false);
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
