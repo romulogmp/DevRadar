981f338dd0898e112d09366052db6aa4e9d7a16b
romulogmp <romulo.mendonca.pereira@everis.com>
2020-01-21 18:39:24 -0300

    Delete, Edit and Github buttons implementation

--- web/src/App.js
+++ web/src/App.js
@@ -1,50 +1,87 @@
-import React, {useState, useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
+import api from './services/api'
 
-import api from './services/api';
-
-import './global.css';
-import './App.css';
+import './global.css'
+import './App.css'
 import './Sidebar.css'
 import './Main.css'
 
-import DevForm from './components/DevForm';
-import DevItem from './components/DevItem';
-
+import DevItem from './components/DevItem'
+import DevForm from './components/DevForm'
+import UpdateForm from './components/UpdateDevForm'
 
 function App() {
-  const [devs, setDevs] = useState([]);
+
+  const [devs, setDevs] = useState([])
+  const [actualDev, setActualDev] = useState('')
+  const [editMode, setEditMode] = useState(false)
+
+  async function loadDevs() {
+    const response = await api.get('/devs');
+    setDevs(response.data.reverse())
+  }
 
   useEffect(() => {
-    async function loadDevs() {
-      const response = await api.get('/devs');
+    loadDevs()
+  }, [])
 
-      setDevs(response.data);
+  async function handleSubmit(data) {
+    const response = await api.post('/devs', data)
+
+    setDevs([response.data, ...devs])
+  }
+
+  async function handleDelete(data) {
+    console.log(data);
+    await api.delete(`/devs?github_username=${data.github_username}`)
+
+    const filterDevs = devs.filter(dev => dev._id !== data._id)
+    setDevs(filterDevs)
+  }
+
+  async function handleUpdate(data) {
+    console.log(actualDev);
+    await api.put(`/devs?github_username=${actualDev.github_username}`, data)
+
+    loadDevs()
+    setMode()
+  }
+
+  function loadMode() {
+    if (editMode) {
+      return (
+        <aside>
+          <strong>Editar</strong>
+          <UpdateForm onUpdateForm={handleUpdate} onCancel={setMode} dev={actualDev} />
+        </aside>
+      )
+    } else {
+      return (
+        <aside>
+          <strong>Cadastrar</strong>
+          <DevForm onSubmit={handleSubmit} />
+        </aside>
+      )
     }
+  }
 
-    loadDevs();
-  }, []);
-
-  async function handleAddDev(data) {
-    const response = await api.post('/devs', data);
-    const hasDev = devs.filter(dev => (dev.github_username === data.github_username));
-    
-    if(hasDev.length === 0) {
-      setDevs([...devs, response.data])
-    }  
+  function setMode(data) {
+    if (!editMode) {
+      setEditMode(true)
+      setActualDev(data)
+    } else {
+      setEditMode(false)
+    }
   }
 
   return (
     <div id="app">
-      <aside>
-        <strong>Cadastrar</strong>
-        <DevForm onSubmit={handleAddDev} />
-      </aside>
-
+      {loadMode()}
       <main>
         <ul>
           {devs.map(dev => (
-            <DevItem key={dev._id} dev={dev} />
-          ))}          
+            <DevItem key={dev._id} dev={dev} onDeleteForm={handleDelete} onUpdateClick={setMode} />
+          ))}
         </ul>
       </main>
     </div>
--- web/src/components/DevItem/index.js
+++ web/src/components/DevItem/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
+import { Edit, Delete, GitHub } from '@material-ui/icons';
+import { Button, ButtonGroup } from '@material-ui/core';
+
 import './styles.css';
 
-function DevItem({dev}) {
+function DevItem({ dev, onDeleteForm, onUpdateClick }) {
+  const { _id, avatar_url, name, techs, github_username, bio } = dev;
+
+  async function handleDelete() {
+    await onDeleteForm({ _id, github_username })
+  }
+
+  async function handleUpdateState() {
+    await onUpdateClick({ _id, github_username })
+  }
   return(
     <li className="dev-item">
       <header>
@@ -13,7 +25,11 @@ function DevItem({dev}) {
         </div>
       </header>
       <p>{dev.bio}</p>
-      <a href={`https://github.com/${dev.github_username}`}>Acessar perfil no Github</a>
+      <ButtonGroup variant="contained" aria-label="contained primary button group">
+        <Button onClick={() => window.open(`https://github.com/${github_username}`, "_blank")} color="primary" size="small" ><GitHub id="github-btn" fontSize="default" /></Button>
+        <Button size="small" color="primary" onClick={handleUpdateState} ><Edit fontSize="default" /></Button>
+        <Button size="small" color="secondary" onClick={handleDelete} ><Delete fontSize="default" /></Button>
+      </ButtonGroup>
     </li>
   )
 }
